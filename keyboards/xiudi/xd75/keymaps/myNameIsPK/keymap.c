38f602b72a0ca77b0a6746cb9ccd8ab69e622e54
myNameIsPk <43566401+myNameIsPK@users.noreply.github.com>
2021-12-29 19:06:00 +0700

    hold nav and mouse layer to go to adjust layer

--- keyboards/xiudi/xd75/keymaps/myNameIsPK/keymap.c
+++ keyboards/xiudi/xd75/keymaps/myNameIsPK/keymap.c
@@ -29,6 +29,7 @@
 
 /* Dual fucction key */
 #define LT_TAB  LT(_ADJ, KC_TAB)
+#define LT_DEL  LT(_ADJ, KC_DEL)
 #define LT_ESC  LT(_NUM, KC_ESC)
 #define LT_SPC  LT(_MSE, KC_SPC)
 #define LT_BSPC LT(_NAV, KC_BSPC)
@@ -63,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LEFT, KC_DOWN, KC_RGHT, KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_QUOT,
         KC_ESC,  LGUI_A,  LALT_S,  LSFT_D,  LCTL_F,  KC_G,    KC_MINS, KC_DEL,  KC_EQL,  KC_H,   RCTL_J,  RSFT_K,  RALT_L,  RGUI_SCLN,KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_BSLS, KC_RBRC, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-        KC_LCTL, KC_LGUI, KC_LALT, LT_TAB,  LT_ESC,  LT_SPC,  KC_HOME, KC_INS,  KC_END,  LT_BSPC,LT_ENT,  KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT, KC_TAB,  LT_ESC,  LT_SPC,  KC_HOME, KC_INS,  KC_END,  LT_BSPC,LT_ENT,  KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT
     ),
 
     /* NUMBER
@@ -133,7 +134,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_INS,  KC_END,  KC_PGUP, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PGDN, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS,  KC_PSCR, _______,
-        _______, _______, _______, _______, _______, KC_TAB,  _______, _______, _______, _______, _______, _______, _______, _______, _______
+        _______, _______, _______, _______, _______, LT_TAB,  _______, _______, _______, _______, _______, _______, _______, _______, _______
     ), 
 
     /* MOUSE
@@ -154,7 +155,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______, 
         _______, _______, KC_BTN3, KC_BTN2, KC_BTN1, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, 
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, LT_DEL,  _______, _______, _______, _______, _______
     ), 
 
     /* ADJUST
