f4e7473d1726a78d3b01edb92deb6cb93b688a9b
myNameIsPk <43566401+myNameIsPK@users.noreply.github.com>
2022-01-28 06:19:57 +0700

    add one shot key in mouse layer

--- keyboards/xiudi/xd75/keymaps/myNameIsPK/config.h
+++ keyboards/xiudi/xd75/keymaps/myNameIsPK/config.h
@@ -18,3 +18,6 @@
 #define TAPPING_TERM 200
 
 #define IGNORE_MOD_TAP_INTERRUPT
+
+#define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
